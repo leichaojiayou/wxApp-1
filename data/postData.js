6f58b22ab3275f0cc1f39ff806f253086e4e3e47
roundDB <1643453298@qq.com>
2017-03-08 23:11:39 +0800

    add play-music

--- data/postData.js
+++ data/postData.js
@@ -12,7 +12,12 @@ var posts = [
         author: '李白2',
         dataTime: '24time',
         detail: '《比利·林恩的中场战事》根据本·芳汀同名小说改编，讲述了在伊拉克战争中的美国士兵比利·林恩与战友战胜归来并被誉为美国英雄，在一场橄榄球公开赛的中场表演过程中，揭露这群士兵在战场上真实经历的故事。',
-        postID: 0
+        postID: 0,
+        music:{
+            url:'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+            title:'此时此刻-许巍',
+            coverImg:'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+        }
     },
     {
         date: 'Sep 18 2016',
@@ -26,7 +31,12 @@ var posts = [
         author: '李白',
         dataTime: '24time',
         detail: '秋季是吃海鲜的最佳时节，这个季节鱼虾肉肥味美，海鲜渔货出产丰富的时期，品种繁多、味道鲜甜，让人忍不住多吃几顿，菊香蟹肥，牡蛎也相当肥美。',
-        postID: 1
+        postID: 1,
+        music:{
+            url:'http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38',
+            title:'朋友-谭咏麟',
+            coverImg:'http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000'
+        }
     },
     
     {
@@ -42,7 +52,12 @@ var posts = [
         author: '李白3',
         dataTime: '24time',
         detail: '黑猫属于猫科动物，泛指毛色通身黑色的猫。古书记载黑猫为镇宅、辟邪、招财之物。黑猫与孟买猫非常的类似，它们的身体也呈现漆黑色。',
-        postID: 2
+        postID: 2,
+        music:{
+            url:'http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38',
+            title:'日日夜夜-齐秦',
+            coverImg:'http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000'
+        }
     },
     {
         //按住alt + shift + f可以格式化代码
@@ -57,7 +72,12 @@ var posts = [
         author: '李白4',
         dataTime: '24time',
         detail: '虚拟现实技术是一种可以创建和体验虚拟世界的计算机仿真系统，它利用计算机生成一种模拟环境，是一种多源信息融合的、交互式的三维动态视景和实体行为的系统仿真使用户沉浸到该环境中。',
-        postID: 3
+        postID: 3,
+        music:{
+            url:'http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38',
+            title:'沉默是金-张国荣',
+            coverImg:'http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000'
+        }
     },
 ];
 
--- pages/news/post-detail/post-detail.js
+++ pages/news/post-detail/post-detail.js
@@ -1,37 +1,108 @@
-var postData=require("../../../data/postData.js");
+var postData = require("../../../data/postData.js");
+var app=getApp();
 Page({
-    data:{
-
+    data: {
+        isPlayingMusic:false
     },
-    onLoad:function(options){
+    onLoad: function (options) {
         //获取文章详情数据
-        var postID=options.id;
-        this.data.nowID=postID;
+        var postID = options.id;
+        this.data.nowID = postID;
         this.setData({
-            postDetail:postData.postList[postID]
+            postDetail: postData.postList[postID]
         })
         //读取\设置缓存记录文章收藏状态
-        var postCollected=wx.getStorageSync("post-collected");
-        if(postCollected){
-            var isCollected=postCollected[postID];
+        var postCollected = wx.getStorageSync("post-collected");
+        if (postCollected) {
+            var isCollected = postCollected[postID];
             this.setData({
-                collected:isCollected
+                collected: isCollected
             })
-        }else{
-            var postCollected={};
-            postCollected[postID]=false;
+        } else {
+            var postCollected = {};
+            postCollected[postID] = false;
             wx.setStorageSync('post-collected', postCollected);
         }
-        
+        if(app.globalData.g_isMusicPlaying){
+            this.setData({
+                isPlayingMusic:true
+            })
+            // this.data.isPlayingMusic=true;
+        }
+        this.setMusicMonitor();
     },
-    collect:function(){
-        var postCollected=wx.getStorageSync("post-collected");
-        var isCollected=postCollected[this.data.nowID];
-        isCollected=!isCollected;
-        postCollected[this.data.nowID]=isCollected;
-        wx.setStorageSync('post-collected',postCollected);
-        this.setData({
-            collected:isCollected
+    setMusicMonitor:function(){
+        var that=this;
+        wx.onBackgroundAudioPlay(function() {
+         that.setData({
+                isPlayingMusic:true
+            });
+         app.globalData.g_isMusicPlaying=true;
         })
+        wx.onBackgroundAudioPause(function() {
+          that.setData({
+                isPlayingMusic:false
+            });
+          app.globalData.g_isMusicPlaying=false;
+        })
+
+    },
+    collect: function () {
+        var postCollected = wx.getStorageSync("post-collected");
+        var isCollected = postCollected[this.data.nowID];
+        isCollected = !isCollected;
+        postCollected[this.data.nowID] = isCollected;
+        wx.setStorageSync('post-collected', postCollected);
+        this.setData({
+            collected: isCollected
+        })
+        wx.showToast({
+            title: isCollected ? "成功收藏" : "取消成功",
+            duration: 1000
+        })
+    },
+    //     onShareAppMessage: function () {
+    //     return {
+    //       title: '分享小程序',
+    //       path: '/page/user?id=123'
+    //     }
+    //   }
+    onMusicTap: function() {
+        // console.log(this.data.nowID);
+        var isPlayingMusic=this.data.isPlayingMusic;
+        var musicDetail=postData.postList[this.data.nowID];
+        // this.setData({
+        //     musicDetail:postData.postList[this.data.nowID]
+        // })
+        if( isPlayingMusic) {
+            wx.pauseBackgroundAudio();
+            this.setData({
+                isPlayingMusic: false
+            });
+        } else{
+             wx.playBackgroundAudio({
+                dataUrl:musicDetail.music.url,
+                title: musicDetail.music.title,
+                coverImgUrl: musicDetail.music.coverImg
+            });
+            this.setData({
+                isPlayingMusic: true
+            });
+          
+        }
+    },
+    // onHide:function(){
+    //     // this.setData({
+    //     //      isPlayingMusic: false
+    //     // })
+    //     console.log("onHide")
+    // },
+    onUnload:function(){
+        // this.setData({
+        //      isPlayingMusic: false
+        // })
+        //当退出某篇文章(页面卸载)重置music图表样式
+        app.globalData.g_isMusicPlaying=false;
+        // console.log("onUnload")
     }
 })
